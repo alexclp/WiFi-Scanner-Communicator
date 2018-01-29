c5062cada09c0d555e0714163b0e109ee9e9c447
Alexandru Clapa <alexandru.clapa@gmail.com>
2018-01-29 12:53:40 +0000

    Changed script a bit to work from an outside folder

--- executer.sh
+++ executer.sh
@@ -1,7 +1,9 @@
-sudo rm src/main/resources/output.txt
-sudo iwlist wlan0 scan | egrep "Cell|ESSID|Signal" >> src/main/resources/output.txt
+# sudo rm src/main/resources/output.txt
+# sudo iwlist wlan0 scan | egrep "Cell|ESSID|Signal" >> src/main/resources/output.txt
 
-mvn install
-mvn compile
-mvn exec:java
-
+# mvn install
+# mvn compile
+# mvn exec:java
+sudo rm /home/pi/Work/Individual\ Project/WiFi-Scanner-Communicator/src/main/resources/output.txt
+sudo iwlist wlan0 scan | egrep "Cell|ESSID|Signal" >> /home/pi/Work/Individual\ Project/WiFi-Scanner-Communicator/src/main/resources/output.txt
+mvn -f /home/pi/Work/Individual\ Project/WiFi-Scanner-Communicator/pom.xml exec:java
